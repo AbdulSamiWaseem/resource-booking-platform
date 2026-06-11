52f5ac17bf7cbbab2109d091351b00347c29969b
Abdul Sami Waseem <abdulsamiwaseem26@gmail.com>
2026-06-11 14:47:58 +0500

    refactor: replace custom CORS middleware with cors package

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const userRoutes = require('./src/routes/userRoutes');
 
 const app = express();
 
-// Enable CORS middleware
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
+app.use(cors());
 app.use(express.json());
 
 // Routes
