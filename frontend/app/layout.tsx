398fa029fb2ab0cfee7b187acc7757df94ab879a
Abdul Sami Waseem <abdulsamiwaseem26@gmail.com>
2026-06-17 17:46:50 +0500

    feat: integrate React Query Devtools

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import type { ReactNode } from "react";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Toaster position="top-center" />
         <QueryClientProvider client={queryClient}>
           {children}
+          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </body>
     </html>
