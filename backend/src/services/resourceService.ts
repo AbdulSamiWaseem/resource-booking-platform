0b927030b33661faaaf5481219631257b7ef72c5
Abdul Sami Waseem <abdulsamiwaseem26@gmail.com>
2026-06-18 11:17:03 +0500

    refactor: updated the resourec resp

--- backend/src/services/resourceService.ts
+++ backend/src/services/resourceService.ts
@@ -5,62 +5,74 @@ export const addResource = async (body: { name: string; description: string }, r
 
   const newResource = await createResource({ name, description });
 
-  resp.success_message = "Resource created successfully";
-  resp.data = {
-    resource: newResource,
+  return {
+    ...resp,
+    success_message: "Resource created successfully",
+    data: {
+      resource: newResource,
+    }
   };
-
-  return resp;
 };
 
 export const getAllResources = async (resp: any) => {
   const resources = await getAllResourcesList();
-  resp.success_message = "Resources retrieved successfully";
-  resp.data = {
-    resources,
+  return {
+    ...resp,
+    success_message: "Resources retrieved successfully",
+    data: {
+      resources,
+    }
   };
-  return resp;
 };
 
 export const getResourceDetailsById = async (id: number, resp: any) => {
   const resource = await findResourceById(id);
   if (!resource) {
-    resp.error = true;
-    resp.error_message = "Resource not found";
-    return resp;
+    return {
+      error: true,
+      error_message: "Resource not found",
+    };
   }
-  resp.success_message = "Resource Details fetched successfully";
-  resp.data = {
-    resource,
+  return {
+    ...resp,
+    success_message: "Resource Details fetched successfully",
+    data: {
+      resource,
+    }
   };
-  return resp;
 };
 
 export const deleteResourceById = async (id: number, resp: any) => {
   const resource = await findResourceById(id);
   if (!resource) {
-    resp.error = true;
-    resp.error_message = "Resource not found";
-    return resp;
+    return {
+      error: true,
+      error_message: "Resource not found",
+    };
   }
   await deleteResource(id);
-  resp.success_message = "Resource deleted successfully";
-  return resp;
+  return {
+    ...resp,
+    success_message: "Resource deleted successfully",
+  };
 };
 
 export const editResourceById = async (id: number, body: { name: string; description: string }, resp: any) => {
   const resource = await findResourceById(id);
   if (!resource) {
-    resp.error = true;
-    resp.error_message = "Resource not found";
-    return resp;
+    return {
+      error: true,
+      error_message: "Resource not found",
+    };
   }
   const updatedResource = await updateResource(id, body);
-  resp.success_message = "Resource updated successfully";
-  resp.data = {
-    resource: updatedResource,
+  return {
+    ...resp,
+    success_message: "Resource updated successfully",
+    data: {
+      resource: updatedResource,
+    }
   };
-  return resp;
 };
 
 
