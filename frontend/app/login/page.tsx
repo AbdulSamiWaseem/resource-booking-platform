14c820ae2904e0fb6eb45cc27de37410982eac01
Abdul Sami Waseem <abdulsamiwaseem26@gmail.com>
2026-06-19 16:54:12 +0500

    refactor: move login mutation into mutuation.tsx

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,19 +1,12 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useMutation } from "@tanstack/react-query";
-import { postApi } from "../services/apiCalls";
-import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-
-interface LoginInput {
-  name: string;
-  email: string;
-}
+import { login, LoginInput } from "../services/mutation";
 
 const schema = z.object({
   name: z.string().min(1, "Name is required"),
@@ -38,20 +31,7 @@ export default function Login() {
     }
   }, [router]);
 
-  const loginMutation = useMutation({
-    mutationFn: (data: LoginInput) => postApi("users", data),
-    onSuccess: (res: any) => {
-      toast.success(res?.message || "Successful!");
-      if (res?.data) {
-        localStorage.setItem("user", JSON.stringify(res.data));
-      }
-      reset();
-      router.push("/dashboard");
-    },
-    onError: (err: any) => {
-      toast.error(err?.response?.data?.message || "An error occurred.");
-    },
-  });
+  const loginMutation = login(reset);
 
   const handleOnSubmit = (data: LoginInput) => {
     loginMutation.mutate(data);
--- frontend/app/services/mutation.tsx
+++ frontend/app/services/mutation.tsx
@@ -26,3 +26,27 @@ export const createResource = () => {
 
   return createMutation;
 };
+
+export interface LoginInput {
+  name: string;
+  email: string;
+}
+
+export const login = (reset: () => void) => {
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: (data: LoginInput) => postApi("users", data),
+    onSuccess: (res: any) => {
+      toast.success(res?.message || "Successful!");
+      if (res?.data) {
+        localStorage.setItem("user", JSON.stringify(res.data));
+      }
+      reset();
+      router.push("/dashboard");
+    },
+    onError: (err: any) => {
+      toast.error(err?.response?.data?.message || "An error occurred.");
+    },
+  });
+};
